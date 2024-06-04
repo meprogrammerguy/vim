b73fb9ccc440df0554f85043a4fb06cb715bbca1
meprogrammerguy <b65535@gmail.com>
2024-06-04 15:49:15 -0500

    have router.sh chew the brave output

--- linux/sh/router.sh
+++ linux/sh/router.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-( sleep 20; brave http://localhost | date >> brave.txt) &
-sudo -E ssh -i ~/.ssh/id_ed25519 -4 -p 1222 -L localhost:80:192.168.0.1:80 jsmith@b65535-olympic.nord "cat info.txt; pwsh"
+( sleep 20; brave http://localhost > /dev/null ) &
+#sudo -E ssh -i ~/.ssh/id_ed25519 -4 -p 1222 -L localhost:80:192.168.0.1:80 jsmith@b65535-olympic.nord "cat info.txt; pwsh;"
+sudo -E ssh -i ~/.ssh/id_ed25519 -4 -p 1222 -L localhost:80:192.168.0.1:80 jsmith@jsmith-win "cat info.txt; pwsh;"
